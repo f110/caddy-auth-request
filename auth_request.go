b1a52e1bfa460a56d47dd06f39e21c1f03b433ce
Fumihiro Itoh <fmhrit@gmail.com>
2017-07-04 10:53:06 +0900

    WIP

--- auth_request.go
+++ auth_request.go
@@ -1,17 +1,30 @@
 package authrequest
 
 import (
-	"fmt"
 	"net/http"
 
 	"context"
 
-	"os"
-
 	"net/url"
+
+	"strings"
+
+	"net"
 
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 )
+
+var hopHeaders = []string{
+	"Connection",
+	"Proxy-Connection",
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Te",
+	"Trailer",
+	"Transfer-Encoding",
+	"Upgrade",
+}
 
 type AuthRequestHandler struct {
 	Next    httpserver.Handler
@@ -42,8 +55,54 @@ func (ar AuthRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) (
 	}
 	backendReq = backendReq.WithContext(r.Context())
 
+	copiedHeaders := false
+	if c := backendReq.Header.Get("Connection"); c != "" {
+		for _, f := range strings.Split(c, ",") {
+			if f = strings.TrimSpace(f); f != "" {
+				if !copiedHeaders {
+					backendReq.Header = make(http.Header)
+					copyHeader(backendReq.Header, r.Header)
+					copiedHeaders = true
+				}
+				backendReq.Header.Del(f)
+			}
+		}
+	}
+
+	for _, h := range hopHeaders {
+		if backendReq.Header.Get(h) != "" {
+			if !copiedHeaders {
+				backendReq.Header = make(http.Header)
+				copyHeader(backendReq.Header, r.Header)
+				copiedHeaders = true
+			}
+			backendReq.Header.Del(h)
+		}
+	}
+
+	if clientIP, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+		if prior, ok := backendReq.Header["X-Forwarded-For"]; ok {
+			clientIP = strings.Join(prior, ", ") + ", " + clientIP
+		}
+		backendReq.Header.Set("X-Forwarded-For", clientIP)
+	}
+
 	res, err := http.DefaultTransport.RoundTrip(backendReq)
-	fmt.Fprintln(os.Stderr, res)
-	fmt.Fprintln(os.Stderr, err)
-	return ar.Next.ServeHTTP(w, r)
+	if err != nil {
+		return ar.Next.ServeHTTP(w, r)
+	}
+	if res.StatusCode == http.StatusOK {
+		return ar.Next.ServeHTTP(w, r)
+	}
+
+	w.WriteHeader(res.StatusCode)
+	return 0, nil
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
 }
--- test_backend/test_backend.go
+++ test_backend/test_backend.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		log.Print(*req)
+		buf, _ := httputil.DumpRequest(req, false)
+		log.Print(string(buf))
+		w.WriteHeader(http.StatusForbidden)
 	})
 	log.Fatal(http.ListenAndServe("127.0.0.1:2015", nil))
 }
